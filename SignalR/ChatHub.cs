8ac91f0c4c5649d1836b58e7f4aba78ac8ca5430
Umar Shareef <Omar.Sharif91@gmail.com>
2020-12-15 05:43:12 +0500

    SignalR Sample

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNet.SignalR;
+
+public class ChatHub : Hub
+{
+    public void Send(string name, string message)
+    {
+        // Call the broadcastMessage method to update clients.
+        Clients.All.broadcastMessage(name, message);
+    }
+}
